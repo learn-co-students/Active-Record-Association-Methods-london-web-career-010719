3e2007f74c17c475da1645e58809bcaca3fd8fa3
kkoomin <minha.koo.9@gmail.com>
2019-01-20 21:18:30 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,9 +8,6 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
-    self.artist = Artist.new(name: "Drake") if self.artist = nil
-    # drake = Artist.find_or_create_by(name: "Drake")
-    # self.artist = drake
-    # ----> The find_or_create_by method checks whether a record with the specified attributes exists. If it doesn't, then create is called
+    self.artist = Artist.new(name: "Drake")
   end
 end
